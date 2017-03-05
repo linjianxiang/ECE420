8fad0c60d4b7986669d12d68cca981601c4e51c7
linjianxiang <l.jxiang@hotmail.com>
2017-03-05 16:12:06 -0700

    openmp

--- pointer_testing.c/pointer_testing.c
+++ pointer_testing.c/pointer_testing.c
@@ -1,13 +1,21 @@
 #include  <stdio.h>
 #include <stdlib.h>
 
+
+
 int main(){
 int a = 10;
 int* b = 100;
+char *s = (char *)("56002500");
+int i = strtol(s, NULL, 16);
+printf("i is %d \n",i);
 
 
-printf("a is %d \n b is %d",a,b);
-printf("a is %f \n b is %f", &a, &b);
+//printf("a is %d \n b is %d",a,b);
+//printf("a is %f \n b is %f", &a, &b);
+
+
+
 getchar();
 return 0;
 }
